461073448c98b3fea4faebf0666305b75c1425a7
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-06-04 13:14:46 -0300

    refactor: refatorando o código, alterando os comandos de parada

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from math import sqrt
+
 nums = list()
 
-n = str(input('insira um valor(n - Para Sair): ')).lower()
+n = str(input('Insira um valor [s - Para Sair]: ')).lower()
 
-while n != 'n':
+while n != 's':
     if n.isdigit():
         nums.append(float(n))
-        print('added')
+        print('adicionado!')
     else:
-        print('not')
+        print('Insira um valor númerico!')
 
-    n = str(input('insira um valor(n - Para Sair): ')).lower()
+    n = str(input('Insira um valor [s - Para Sair]: ')).lower()
 
 media = float(sum(nums)/len(nums))
-
 desvio = [i - media for i in nums]
 desvioq = list(map(lambda x : x ** 2, desvio))
 variancia = sum(desvioq)/len(desvioq)
@@ -21,12 +21,11 @@ varianciav = sum(desvioq) / (len(desvioq) - 1)
 desviop = sqrt(variancia)
 desviopv = sqrt(varianciav)
 
-print('='*40)
+print('=' * 60)
 print('media:', media)
 print('desvio:', desvio)
 print('variancia', variancia)
 print('desvio padrao', desviop)
 print('variancia viciada:',varianciav)
 print('desvio padrao viciado:', desviopv)
-print('='*40)
-
+print('=' * 60)
